86aee64002dd94b318adf57cb58a357ade55bcec
Alnur_Faisal <alnurfaisal@outlook.com>
2018-05-01 00:51:42 +0800

    Added and configure firebase with dotenv

---
+++ src/configFirebase.js
@@ -0,0 +1,14 @@
+import Rebase from "re-base";
+import * as firebase from "firebase";
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURI,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDER
+};
+console.log(config);
+const app = firebase.initializeApp(config);
+export const base = Rebase.createClass(app.database());
